1244bf25d5b07c70a58627170b0432ce45753815
saeendamghanian <50122494+saeendamghanian@users.noreply.github.com>
2019-07-20 16:44:33 +0200

    Added the codes of Million

--- script.js
+++ script.js
@@ -1,6 +1,36 @@
 var searchBtn = document.querySelector("#searchItBtn");
 searchBtn.addEventListener("click", getInput);
 
-function getInput() {
-  return document.querySelector("#search-box").value;
+function getInput(e) {
+  var userInput = document.querySelector("#search-box").value;
+  fetchData(userInput);
+  document.querySelector("#search-box").value = "";
+}
+
+function fetchData(text) {
+  fetch("https://indian-cities-api-nocbegfhqg.now.sh/cities") // take json
+    .then(response => response.json()) // if success transform json in JS object
+    .then(data => {
+      //console.log(data);
+      for (var i = 0; i < data.length; i++) {
+        var city = "";
+        if (data[i].City === text) {
+          //console.log(data[0]);
+          city +=
+            "City name: " +
+            data[i].City +
+            "<br>" +
+            "District: " +
+            data[i].District +
+            "<br>" +
+            "State: " +
+            data[i].State;
+
+          var ouData = document.createElement("p");
+          ouData.innerHTML = city;
+          ouData.setAttribute("class", "searchResult");
+          document.body.appendChild(ouData);
+        }
+      }
+    });
 }
